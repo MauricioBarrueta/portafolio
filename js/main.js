d5d6a8848c081466c12707ed4156eb9b94ba8628
Mauricio <mauba22@outlook.com>
2023-05-05 08:48:30 -0600

    Updates

--- js/main.js
+++ js/main.js
@@ -102,19 +102,13 @@ const showAndHideProjects = () => {
     var cardsHidden = document.getElementById("hiddenCards");
     var btnShowHiddenCards = document.getElementById("show-more-btn");
     if (cardsVisible.style.display === "none") {
-        btnShowHiddenCards.innerHTML = "<i class='fa-solid fa-angle-down fa-bounce'></i>";
-        btnShowHiddenCards.setAttribute("data-title", "Mostrar más - Show more");
-        cardsVisible.style.display = "flex";
-        cardsHidden.style.display = "none";
+        btnShowHiddenCards.innerHTML = "<i class='fa-solid fa-angle-down fa-bounce'></i>", btnShowHiddenCards.setAttribute("data-title", "Mostrar más - Show more");
+        cardsVisible.style.display = "flex", cardsHidden.style.display = "none";
     } else {
         cardsVisible.style.display = "none";
-        btnShowHiddenCards.innerHTML = "<i class='fa-solid fa-angle-up fa-bounce'></i>";
-        btnShowHiddenCards.setAttribute("data-title", "Mostrar menos - Show less");
-        cardsHidden.style.display = "flex";
-        cardsHidden.style.flexWrap = "wrap";
-        cardsHidden.style.alignContent = "center";
-        cardsHidden.style.justifyContent = "space-around";
-        cardsHidden.style.gap = "15px";
+        btnShowHiddenCards.innerHTML = "<i class='fa-solid fa-angle-up fa-bounce'></i>", btnShowHiddenCards.setAttribute("data-title", "Mostrar menos - Show less");
+        cardsHidden.style.display = "flex", cardsHidden.style.flexWrap = "wrap", cardsHidden.style.alignContent = "center",
+        cardsHidden.style.justifyContent = "space-around", cardsHidden.style.gap = "15px";
     }
     if (btnShowHiddenCards.getAttribute("data-title", "Mostrar más - Show more") && cardsHidden.style.display === "none") {
         document.querySelector('#Projects').scrollIntoView({ behavior: 'smooth' });
